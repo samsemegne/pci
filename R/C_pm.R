4dc33bb6812bd746b66748a01937b678e71f8b1f
unknown <samsemegne@gmail.com>
2024-06-11 21:36:27 +0200

    Adds examples to C_pm()

--- R/C_pm.R
+++ R/C_pm.R
@@ -20,6 +20,10 @@
 #' 0.
 #' @returns `double`.
 #' @references `r get_montgomery_ref_str()`
+#' @examples
+#' C_pm(mu = 3., sigma = 1., target = 3., lsl = 0., usl = 6., dl = 6.)
+#' # [1] 1
+#'
 #' @export
 C_pm = function(mu, sigma, target, lsl, usl, dl) {
   if (!vek::is_num_vec_z(mu))
