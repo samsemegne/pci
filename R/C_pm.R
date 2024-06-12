f2a148cee696d5fc861c146d8739e02f8083ce02
unknown <samsemegne@gmail.com>
2024-06-12 20:28:55 +0200

    Adds data example to C_pm() examples

--- R/C_pm.R
+++ R/C_pm.R
@@ -21,8 +21,10 @@
 #' @returns `double`.
 #' @references `r get_montgomery_ref_str()`
 #' @examples
-#' C_pm(mu = 3., sigma = 1., target = 3., lsl = 0., usl = 6., dl = 6.)
-#' # [1] 1
+#' set.seed(1L)
+#' y = rnorm(n = 30L, mean = 3., sd = 1.)
+#' C_pm(mu = mean(y), sigma = sd(y), target = 3., lsl = 0., usl = 6., dl = 6.)
+#' # [1] 1.077827
 #'
 #' @export
 C_pm = function(mu, sigma, target, lsl, usl, dl) {
